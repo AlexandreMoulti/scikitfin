63d85cbd8403b0134cab77ece1f101376771077d
Ange Romuald OSSOHOU <arossohou@gmail.com>
2022-11-27 23:36:32 +0100

    ZC and Swaption prices

--- scikitfin/code.py
+++ scikitfin/code.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy import interpolate
 import matplotlib.pyplot as plt
-from typing import Literal, Union, List, Any
+from typing import Literal, Union, List, Any, Tuple, Callable
 from numpy.typing import ArrayLike
 from math import *
 from scipy.stats import norm
@@ -114,7 +114,7 @@ class IRCurve(object):
             instantaneous forwards.
 
         """
-        if maturities[0]!=0 :
+        if np.array(maturities)[0]!=0 :
             maturities = np.concatenate(([0], maturities))
             zc_prices  = np.concatenate(([1], zc_prices))
         return np.diff(-np.log(zc_prices))/np.diff(maturities)
@@ -184,10 +184,10 @@ class IRCurve(object):
             swap rate curve.
 
         """
-        if maturities[0] != 0:
+        if np.array(maturities)[0] != 0:
             maturities = np.concatenate(([0],maturities))
         dt = np.diff(maturities)
-        return (1-zc_prices)/(dt*zc_prices).cumsum()
+        return (1-np.array(zc_prices))/(dt*zc_prices).cumsum()
    
     @staticmethod
     def swap_rates_to_zc_prices(maturities : ArrayLike, 
@@ -209,7 +209,8 @@ class IRCurve(object):
             zero-coupon curve.
 
         """
-        zc = [1/((1+swap_rates[0])** maturities[0])]
+        swap_rates = np.array(swap_rates)
+        zc = [1/((1+swap_rates[0])** np.array(maturities)[0])]
         dt = np.diff(maturities)
         for idx, rate in enumerate(swap_rates[1:]):
             zc_temp = (1 - np.sum(zc* dt[:idx+1] * rate)) / ( 1 + dt[idx]*rate)
@@ -240,7 +241,7 @@ class IRCurve(object):
 
     @staticmethod
     def zc_prices_to_acturial_rates( maturities : ArrayLike, 
-                                     actuarial_rates : ArrayLike
+                                     zc_prices : ArrayLike
                                    ) -> np.ndarray:
         """
         Computes the zero-coupon curve from the actuarial rate curve
@@ -249,16 +250,16 @@ class IRCurve(object):
         ----------
         maturities : ArrayLike
             maturities of the curve.
-        actuarial_rates : ArrayLike
-            actuarial rates.
+        zc_prices : ArrayLike
+             zc_prices.
 
         Returns
         -------
         np.ndarray
-            zero-coupon prices of the curve.
+            actuarial rates.
 
         """
-        pass
+        return np.power(1/np.array(zc_prices), 1/np.array(maturities))
     
     @classmethod
     def data_to_curve(cls, 
@@ -300,7 +301,7 @@ class IRCurve(object):
 
     def plot(self,
              method : Literal['ZeroCoupon', 'Swap','ActuarialRate', 'Yield'] = 'Yield'
-            ) -> Any:
+            ) -> None:
         """
         Plots the curve
 
@@ -311,20 +312,21 @@ class IRCurve(object):
 
         Returns
         -------
-        Any
+        None
             plot of the curve.
 
         """
         
         if method == 'Swap':
-            values = self.price_zcs_to_swap_rates(self.maturities, self.price_zcs)
+            values = self.zc_prices_to_swap_rates(self.maturities, self.price_zcs)
         elif method == 'ActuraialRate':
-            values = self.price_zcs_to_acturial_rates(self.maturities, self.price_zcs)
+            values = self.zc_prices_to_acturial_rates(self.maturities, self.price_zcs)
         elif method == 'Yield':
             values = self.zc_prices_to_yields(self.maturities, self.price_zcs)
         else:
             values=self.price_zcs
-        return plt.plot(self.maturities, values)
+        plt.plot(self.maturities, values)
+        plt.show()
 
 
 
@@ -370,7 +372,9 @@ class DiscretisationGrid(object):
 
 class IRGauss1FConst(object):
     
-    def __init__(self, kappa, sigma, func_instant_forwards=lambda x:0, func_spot_zc=lambda x:1):
+    def __init__(self, kappa : float, sigma: float, func_instant_forwards : Callable[[ArrayLike], np.ndarray],
+                   func_spot_zc : Callable[[ArrayLike], np.ndarray] 
+                ) -> None:
         """
         Interest Rates Gaussian 1 Factor model with constant parameters
 
@@ -390,56 +394,260 @@ class IRGauss1FConst(object):
         """
         self.kappa  = kappa
         self.sigma  = sigma
-        self.func_instant_forwards = func_instant_fowards
+        self.func_instant_forwards = func_instant_forwards
         self.func_spot_zc = func_spot_zc
 
     @np.vectorize
     @staticmethod
-    def G(tau, kappa, sigma):
-        if tau<1e-6:
-            return tau
-        else:
-            return (1-np.exp(-kappa*tau))/kappa
+    def G(tau : ArrayLike, kappa : float) -> np.ndarray:
+        """
+        function used to compute ZC prices
+
+        Parameters
+        ----------
+        tau : ArrayLike
+            time steps.
+        kappa : float
+            parameters of the model.
+
+        Returns
+        -------
+        np.ndarray.
+
+        """
+        tau = np.array(tau)
+        return 1e-6 if tau < 1e-6 else (1-np.exp(-kappa*tau))/kappa
+     
     
     @staticmethod
-    def variance(t, kappa, sigma):
-        return 0.5*sigma**2*self.G(2*t, kappa, sigma)
+    def variance(t : float, kappa : float, sigma : float) -> float:
+        """
+        The variance of the stochastic process x 
+
+        Parameters
+        ----------
+        t : float
+            current time.
+        kappa : float
+            parameters of the model.
+        sigma : float
+            volatility of the short rate
+        Returns
+        -------
+        float.
+        """
+        return 0.5 * sigma**2 * IRGauss1FConst.G(2*t, kappa)
     
+    @np.vectorize
     @staticmethod
-    def mean(t, kappa, sigma):
-        return 0.5*sigma**2*self.G(t, kappa, sigma)**2
+    def zc_bond_volatility(t : float ,T : ArrayLike , kappa : float, sigma : float) -> np.ndarray:
+        """
+        Volotility of ZC  
+
+        Parameters
+        ----------
+        t : float
+            current time.
+        T : ArrayLike 
+            expiry of the ZC bond 
+        kappa : float
+            parameters of the model.
+        sigma : float
+            volatility of the short rate
+        Returns
+        -------
+        np.ndarray.
+        """
+        T = np.array(T)
+        return 0.5 * sigma**2 * IRGauss1FConst.G(T, kappa)**2 * IRGauss1FConst.G(2*(T-t), kappa)
+
+    @np.vectorize
+    @staticmethod
+    def mean(t : float ,x: float ,kappa: float ,sigma : float) -> np.ndarray:
+        """
+        Mean of the stochastic process x 
+
+        Parameters
+        ----------
+        t : float
+            current time.
+        x : float
+            stochastic process x ( x = r - f )
+        kappa : float
+            parameters of the model.
+        sigma : float
+            volatility of the short rate
+        Returns
+        -------
+        nd.ndaray.
+        """
+        return  x * np.exp(-kappa*t) + 0.5 * sigma**2 * IRGauss1FConst.G(t, kappa)**2 
     
-    @staticmethod
-    def price_zc(x, t, T, params):
-        return self.func_spot_zc(T)/self.func_spot_zc(t)*np.exp(-x*self.G(T-t, kappa, sigma)-0.5*self.variance(t, params)*self.G(T-t, kappa, sigma)**2)
+    def price_zc(self,x : float, t : float, T: ArrayLike , kappa: float, sigma: float) -> np.ndarray:
+        """
+        ZC price
+
+        Parameters
+        ----------
+        x : float
+            stochastic process x ( x = r - f )
+        t : float
+            current time.
+        T : ArrayLike 
+            expiry of the ZC bond 
+        kappa : float
+            parameters of the model.
+        sigma : float
+            volatility of the ZC
+        Returns
+        -------
+        np.ndarray.
+        """
+        T = np.array(T)
+        return (self.func_spot_zc(T)/self.func_spot_zc(t)) * \
+               np.exp(-x * IRGauss1FConst.G(T-t, kappa) - 0.5 * IRGauss1FConst.variance(t, kappa, sigma) \
+                * IRGauss1FConst.G(T-t, kappa)**2)
 
     
     def fit(self, ircurve, swaptiondata):
         #calibrates kappa sigma to the data
         pass
     
+    def black_tools(self, x:float, t:float, maturity: ArrayLike, 
+                    tenor: ArrayLike, K: ArrayLike, kappa: float, sigma : float) -> Tuple :
+        """
+        black tools
+
+        Parameters
+        ----------
+        x : float
+            stochastic process x ( x = r - f )
+        t : float
+            current time.
+        maturity: ArrayLike
+            expiry of the option
+        tenor: ArrayLike
+            expiry of the zc underlying
+        K : ArrayLike
+            strike of the option
+        kappa : float
+            parameters of the model.
+        sigma : float
+            volatility of the short rate
+        Returns
+        -------
+        np.ndarray.
+        """
+        maturity = np.array(maturity)
+        v         = IRGauss1FConst.zc_bond_volatility(t,maturity,kappa,sigma)
+        P_up      = self.price_zc(x, t, maturity + tenor, kappa, sigma)
+        P_down    = self.price_zc(x, t, maturity, kappa, sigma)
+        d_positif = (np.log(P_up/(K * P_down)) + v/2 ) / np.sqrt(v)
+        d_negatif = (np.log(P_up/(K * P_down)) - v/2 ) / np.sqrt(v)
+
+        return P_up,P_down,d_positif,d_negatif
+
+    def price_zc_call(self, x:float, t:float, maturity: ArrayLike, tenor: ArrayLike,
+                       K: ArrayLike, kappa : float, sigma : float) -> np.ndarray:
+        """
+        price of call option on ZC
+        P185
+        Parameters
+        ----------
+        x : float
+            stochastic process x ( x = r - f )
+        t : float
+            current time.
+        maturity: ArrayLike 
+            expiry of the option
+        tenor: ArrayLike
+            expiry of the zc underlying
+        K : ArrayLike
+            strike of the option
+        kappa : float
+            parameters of the model.
+        sigma : float
+            volatility of the short rate
+        Returns
+        -------
+        np.ndaaray.
+        """
+        P_up, P_down, d_positif, d_negatif = self.black_tools(x,t,maturity,tenor,K,kappa,sigma)
+
+        return P_up * norm.cdf(d_positif) - P_down * K * norm.cdf(d_negatif)
     
-    @staticmethod
-    def price_zc_call(x:float, t:float, maturity:float, tenor:float, K:float, kappa, sigma) -> float:
-        """
-        p185
-
-        """
-        pass
     
-    @staticmethod
-    def price_zc_put(x:float, t:float, maturity:float, tenor:float, K:float, kappa, sigma) -> float:
+    def price_zc_put(self, x:float, t:float, maturity: ArrayLike, 
+                      tenor: ArrayLike, K: ArrayLike, kappa: float, sigma: float) -> np.ndarray:
         """
+        price of put option on ZC
         p185
+        Parameters
+        ----------
+        x : float
+            stochastic process x ( x = r - f )
+        t : float
+            current time.@
+        maturity: ArrayLike 
+            expiry of the option
+        tenor: ArrayLike
+            expiry of the zc underlying
+        K : ArrayLike
+            strike of the option
+        kappa : float
+            parameters of the model.
+        sigma : float
+            volatility of the short rate
+        Returns
+        -------
+        np.ndarray.
+        """
+        
+        P_up, P_down, d_positif, d_negatif = self.black_tools(x,t,maturity,tenor,K,kappa,sigma)
+
+        return  P_down * K * norm.cdf(-d_negatif) -  P_up * norm.cdf(-d_positif) 
+
+    def price_swaption(self,x : float, t : float, schedule : ArrayLike,
+                      strike : float, kappa : float, sigma: float) -> float:
 
         """
-        pass
+        price of swaption
 
-    @staticmethod
-    def price_swaption(x, t, schedule, strike, kappa, sigma) -> float:
+        Parameters
+        ----------
+        x : float
+            stochastic process x ( x = r - f )
+        t : float
+            current time.
+        schedule: Tuple 
+            Observation dates (T0, T1, ...) 
+        strike: float
+            strike of the option
+        kappa : float
+            parameters of the model.
+        sigma : float
+            volatility of the short rate
+        Returns
+        -------
+        float.
+        """
         #p421
         #schedule = (t0,t1,...,tN)
-        pass
+
+        steps_time   = np.diff(schedule)
+        payment_date = np.array(schedule)[1:]
+        Tn     = payment_date[-1]
+
+        def root_function(x):
+            output = -1 + self.price_zc(x,t,Tn,kappa,sigma) + np.sum(strike * steps_time * self.price_zc(x,t,payment_date,kappa,sigma))    
+            return output
+
+        solution = optimize.root(root_function, 1)
+        optimal_x = solution.x
+        K  = self.price_zc(optimal_x,t, payment_date,kappa,sigma)
+        Vswaption = np.sum(steps_time * K *  self.price_zc_put(x,t,t,payment_date -t,K,kappa,sigma)) + self.price_zc_put(x,t,t,Tn-t,K[-1],kappa,sigma)
+
+        return Vswaption
     
     def simulate(self, grid, dN):
         """
@@ -476,7 +684,17 @@ class IRGauss1FConst(object):
 
 
 myc= IRCurve([0.5, 1,2,3,4,5,6], [0.210, 0.03,0.0288,0.0292,0.0295,0.0298,0.03],'Swap')
-#myc= IRCurve([0.5, 1,2,3,4,5,6], [0.02]*6,'Swap')
-print(myc)
+kappa = 0.5
+sigma = 0.2 
+model = IRGauss1FConst(kappa,sigma,myc.func_instant_forwards, myc.func_spot_zc)
+print(model.func_instant_forwards(10))
+
+
+print(IRGauss1FConst.mean(0,0.2,kappa,sigma))
+zc_prices = model.price_zc(0.2, 0, [1,2,3], kappa, sigma)
+print(zc_prices)
+
+swaption = model.price_swaption(0.2, 0, (1,1.5,2,2.5,3), 0.02 ,kappa, sigma) 
+print("swaption", swaption)
 print(myc.func_instant_forwards(np.linspace(0,10,20+1)))
-myc.plot()
+#myc.plot()
