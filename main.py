15a7bfd6b281fd7e785760a3a32b6310ef7799bd
Alexandre Moulti <7243315+AlexandreMoulti@users.noreply.github.com>
2023-02-14 18:37:46 +0100

    correction of bond volatility formula

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ data_curve = pd.read_csv('courbe.csv')
 
 maturities = data_curve['maturity'].to_numpy()
 discounts = data_curve['Discount'].to_numpy()
+yields = np.ones(maturities.shape[0])*0.03
 print(ircurve.zc_prices_to_actuarial_rates(maturities, discounts))
 #actuarial_rates = np.array([0.01] * 10)
 #swaptiondic = {(1,1):0.1, (10,10):0.05}
 
 
 ######## market structures ##########################################
-irc = InterestRateCurve(maturities, discounts, 'ZeroCoupon')
+irc = InterestRateCurve(maturities, yields, 'Yield')
 #swapsurface = SwaptionSurface(irc, swaptiondic, 'price')
 #print(irc.zc_prices)
 
@@ -33,18 +34,24 @@ grid=0 #use grid object
 x=0
 t=0
 maturity =10
-kappa = 0.03
+tenor = 5
+kappa = 0.1
 sigma=0.01
 schedule=np.arange(10,15.5,0.5)
-strike=0.028
+strike=0.03
 hw = HullWhite(kappa, sigma)
 hw.fit(irc)
 print(hw.price_zc_spot(10))
 print(hw.price_zc(x,t,10,kappa, sigma))
 print('swaption price = {}'.format(hw.price_swaption(x, t, schedule, strike, kappa, sigma)))
+
+strike = 0.5
+print('zc call price : {}'.format(hw.price_zc_call(0,0,maturity, tenor, strike, kappa, sigma)))
  #prix spot = prix market
 # hw.swpation(maturity,  tenor) = prix swap manager
 
 #plus tard
 #hw.fit(ircurve, swaptiondata)
 #new_short_rates = hw.simulate()
+
+print(irc.func_zc_prices(1))
