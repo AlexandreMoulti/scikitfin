9068b7e5354bfee27814c9db3287a673b1484699
alexandre moulti <7243315+AlexandreMoulti@users.noreply.github.com>
2023-09-05 14:54:45 +0200

    Update ircurve.py

--- scikitfin/markets/ircurve.py
+++ scikitfin/markets/ircurve.py
@@ -3,15 +3,12 @@ import pandas as pd
 from scipy import interpolate
 import matplotlib.pyplot as plt
 from typing import Literal, Union, List, Any, Tuple, Callable
-from numpy.typing import ArrayLike
+from numpy.typing import ArrayLike, NDArray
 from math import *
-from scipy.stats import norm
-from scipy import optimize
-
 
 ###############################################################################
 #
-# STATIC METHOHDS
+# HELPER FUNCTIONS
 #
 ###############################################################################
 
@@ -132,13 +129,12 @@ def swap_rates_to_zc_prices(maturities: np.ndarray,
     np.ndarray
         zero-coupon curve.
     """
-    swap_rates = swap_rates
-    zc = [1 / ((1 + swap_rates[0]) ** maturities[0])]
+    zc = np.ones(swap_rates.shape)
+    zc[0] = 1 / ((1 + swap_rates[0]) ** maturities[0])
     dt = np.diff(maturities)
     for idx, rate in enumerate(swap_rates[1:]):
-        zc_temp = (1 - np.sum(zc * dt[:idx + 1] * rate)) / (1 + dt[idx] * rate)
-        zc.append(zc_temp)
-    return np.array(zc)
+        zc[idx+1] = (1 - np.sum(zc * dt[:idx + 1] * rate)) / (1 + dt[idx] * rate)
+    return zc
 
 def actuarial_rates_to_zc_prices(maturities: np.ndarray,
                                  actuarial_rates: np.ndarray
@@ -178,30 +174,9 @@ def zc_prices_to_actuarial_rates(maturities: np.ndarray,
     """
     return np.power(zc_prices, -1 / maturities) - 1
 
-
-def forward_swap_rate(expiry:float, tenor:float, func_zc_prices, dt: float = 0.5) -> float:
-    """
-    compute the forward swap rate
-
-    Convention : the underlying swap pays coupon eac semester
-    Parameters
-    ----------
-    array_of_tuple : ( expiry, tenor)
-    dt : float
-        time step convention for the swap
-
-    Returns
-    -------
-    """
-    maturities = np.arange(expiry + dt, expiry + tenor + dt, dt)
-    annuity_factor = np.sum(dt * func_zc_prices(maturities))
-    return (func_zc_prices(expiry) - func_zc_prices(tenor + expiry)) / annuity_factor
-
-forward_swap_rate = np.vectorize(forward_swap_rate, excluded=['func_zc_prices'])
-
 ###############################################################################
 #
-# INTEREST RATES
+# INTEREST RATES CLASS
 #
 ###############################################################################
 
@@ -209,8 +184,8 @@ forward_swap_rate = np.vectorize(forward_swap_rate, excluded=['func_zc_prices'])
 class InterestRateCurve(object):
 
     def __init__(self,
-                 maturities: np.ndarray,
-                 data: np.ndarray,
+                 maturities: np.ndarray = np.arange(51),
+                 data: np.ndarray = np.ones(51),
                  method: Literal['ZeroCoupon', 'SwapRate', 'ActuarialRate', 'Yield'] = 'ZeroCoupon'
                  ) -> None:
         """
@@ -235,6 +210,8 @@ class InterestRateCurve(object):
         elif method == 'Yield':
             self.zc_prices: np.ndarray = yields_to_zc_prices(maturities, data)
         self.instant_forwards: np.ndarray = zc_prices_to_inst_forwards(self.maturities, self.zc_prices)
+        # vectorized methods
+        self.annuity=np.vectorize(self._annuity, excluded=['self'])
 
     def __str__(self) -> str:
         """
@@ -250,7 +227,7 @@ class InterestRateCurve(object):
     ########################## PROPERTY  ########################################################################
 
     @property
-    def func_instant_forwards(self):
+    def function_instant_forward(self):
         if self.maturities[0] != 0:
             maturities = np.concatenate(([0], self.maturities))
         else:
@@ -260,7 +237,7 @@ class InterestRateCurve(object):
                                     fill_value=(self.instant_forwards[0], self.instant_forwards[-1]))
 
     @property
-    def func_integ_forwards(self):
+    def function_integral_fowards(self):
         if self.maturities[0] != 0:
             maturities = np.concatenate(([0], self.maturities))
         else:
@@ -270,16 +247,45 @@ class InterestRateCurve(object):
                                     kind='linear', fill_value='extrapolate')
 
     @property
-    def func_zc_prices(self):
-        if self.maturities[0] != 0:
-            maturities = np.concatenate(([0], self.maturities))
-        else:
-            maturities = self.maturities
-        dt = np.diff(maturities)
-        return lambda t: np.exp(-self.func_integ_forwards(t))
+    def function_zc_price(self):
+        return lambda t: np.exp(-self.function_integral_fowards(t))
 
     ########################## METHODS  ########################################################################
+    def instant_forward(self, maturities):
+        return self.function_instant_forward(maturities)
+    
+    def yields(self, maturities):
+        return zc_prices_to_yields(maturities, self.zc_price(maturities))
+    
+    def actuarial_rate(self, maturities):
+        return zc_prices_to_actuarial_rates(maturities, self.zc_prices(maturities))
 
+    def zc_price(self,
+                 maturities: ArrayLike
+                ) -> ArrayLike:
+        return self.function_zc_price(maturities)
+
+    def _annuity(self,
+                 expiry: float,
+                 tenor: float,
+                 dt: float = 0.5
+                ) -> float:
+        maturities = np.arange(expiry + dt, expiry + tenor + dt, dt)
+        return np.sum(dt * self.zc_price(maturities))
+    
+    def swap_rate(self,
+                  tenor: ArrayLike,
+                  dt: float = 0.5
+                 ) -> ArrayLike:
+        return (1 - self.zc_price(tenor)) / self.annuity(0, tenor, dt)
+    
+    def forward_swap_rate(self,
+                          expiry: ArrayLike,
+                          tenor: ArrayLike,
+                          dt: float = 0.5
+                         ) -> float:
+        return (self.zc_price(expiry) - self.zc_price(tenor + expiry)) / self.annuity(expiry, tenor, dt)
+            
     def plot(self,
              method: Literal['ZeroCoupon', 'Swap', 'ActuarialRate', 'Yield'] = 'Yield'
              ) -> None:
@@ -305,45 +311,3 @@ class InterestRateCurve(object):
             values = self.zc_prices
         plt.plot(self.maturities, values)
         plt.show()
-
-
-    def forward_swap_rate(self, expiry:float, tenor:float, dt: float = 0.5) -> float:
-        """
-        compute the forward swap rate
-
-        Convention : the underlying swap pays coupon eac semester
-        Parameters
-        ----------
-        array_of_tuple : ( expiry, tenor)
-        dt : float
-            time step convention for the swap
-
-        Returns
-        -------
-        """
-        return forward_swap_rate(expiry, tenor, self.func_zc_prices, dt)
-
-    ########################## CLASS METHODS  ########################################################################
-
-    @classmethod
-    def data_to_curve(cls,
-                      data_file_path: str,
-                      method: Literal['ZeroCoupon', 'Swap', 'ActuarialRate', 'Yield']
-                      ) -> Any:
-        """
-        Alternative constructor from tabular data
-        Parameters
-        ----------
-        data_file_path : str
-            path of the file.
-        method : Literal['ZeroCoupon', 'Swap','ActuarialRate', 'Yield']
-            method of description of the curve.
-        Returns
-        -------
-        Any
-            Curve.
-        """
-        pd_curve = pd.read_csv(data_file_path)
-        maturities = pd_curve.iloc[:, 0].to_numpy()
-        data = pd_curve.iloc[:, 1].to_numpy()
-        return IRCurve(maturities, data, method)
